2a7a64268c96f94ea9bf28dbd718c50496084790
Mahmoud Abd Al-Ghany <Av0peac@hotmail.com>
2018-09-08 21:09:18 +0200

    Create UVA-#294-Divisors.cpp

---
+++ UVA-#294-Divisors.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long int g(int x) {
+	long long cnt = 0, i;
+	for(i = 1; i*i < x; i++) {
+		if(x % i == 0) {
+			cnt += 2;
+		}
+	}
+	if(i*i == x) cnt++;
+	return cnt;
+}
+
+int main() {
+	long long int U, L, TC;
+	cin >> TC;
+	while(TC--) {
+		cin >> U >> L;
+		long long int d = 0, maxN = U;
+		for(int i = U; i <= L; i++) {
+			d = max(d, g(i));
+			maxN = g(i) > g(maxN) ? i : maxN;
+		}
+		printf("Between %llu and %llu, %llu has a maximum of %llu divisors.\n", U, L, maxN, d);
+	}
+}
