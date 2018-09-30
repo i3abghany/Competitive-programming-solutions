8075885a49c14254ad5ccf648c74c14b5a508dab
Mahmoud Abd Al-Ghany <Av0peac@hotmail.com>
2018-09-30 20:42:28 +0200

    .

---
+++ CODEFORCES/B-Mahmoud-and-a-Triangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+typedef long long int ll;
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    ll a[n];
+    for(int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    for(int i = 0; i < n - 2; i++) {
+        if(a[i] + a[i + 1] > a[i + 2]) {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+}
